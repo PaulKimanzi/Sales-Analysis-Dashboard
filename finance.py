24542aefe383c89d0f32bfaba58aff2fd0974c4a
Paul Kimanzi <79704376+PaulKimanzi@users.noreply.github.com>
2026-06-27 23:35:22 +0300

    Create financial analysis dashboard in finance.py
    Set up a financial analysis dashboard using Streamlit and Plotly.

---
+++ finance.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+print("my name")
+st.markdown(
+    "<h1 style='color:blue;'>Financial Analysis Dashboard</h1>",
+    unsafe_allow_html=True
+)
+
+df=pd.read_excel(r"C:\\Users\PC\\Downloads\\Product-Sales-Region.xlsx")
+st.write(df.head())
+fig = px.pie(
+    df,
+    names="Product",
+    values="Quantity",
+    title="Sales by Category",
+    hole=0.3  # Optional: creates a donut chart
+)
+
+st.plotly_chart(fig, use_container_width=True)
