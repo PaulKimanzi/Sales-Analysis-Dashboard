b7766e0889b6bbce29355a473f3c169171efe832
Paul Kimanzi <79704376+PaulKimanzi@users.noreply.github.com>
2026-06-28 10:08:05 +0300

    Refactor markdown rendering and clean up code
    Removed unnecessary line breaks and streamlined the markdown rendering for the dashboard title.

--- finance.py
+++ finance.py
@@ -2,18 +2,16 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-
-st.markdown(
-    "<h1 style='color:blue;'>Financial Analysis Dashboard</h1>",
-    unsafe_allow_html=True
-)
+st.markdown("<h1 style='color:blue;'>Financial Analysis Dashboard</h1>", unsafe_allow_html=True )
 #import the sales Data
 df=pd.read_excel("Product-Sales-Region.xlsx")
-st.write(df.head())
 
 #Add calculated colums into the dataframe
 df["OrderDate"] = pd.to_datetime(df["OrderDate"])
 df["Month"]=df["OrderDate"].dt.month
+
+#Exploratory Data Analysis
+st.write(df.head())
 #Create a Pie chart of Sales by Product Category
 fig = px.pie(df, names="Product", values="Quantity", title="Sales by Category",
     hole=0.3  #to create a donut chart
